3a2f8e3a8ab721b1bcb2307859fbd54d0e85a73c
Llarence <1010kai.t1010@gmail.com>
2026-02-21 00:23:14 -0800

    fix sleep

--- src/util.cpp
+++ src/util.cpp
@@ -2,21 +2,27 @@
 
 #include <hardware/watchdog.h>
 
-bool sleep(Millis target_time) {
+#define HALF_WATCHDOG_MS (WATCHDOG_MS / 2)
+
+// Not super precise
+// Feeds the watchdog while sleeping
+void sleep(Millis target_time) {
   watchdog_update();
 
-  while (target_time > (WATCHDOG_MS / 2)) {
-    delay(WATCHDOG_MS / 2);
+  // We enter a loop to feed the watchdog while sleeping
+  while (target_time > HALF_WATCHDOG_MS) {
+    target_time -= HALF_WATCHDOG_MS;
+    delay(HALF_WATCHDOG_MS);
     watchdog_update();
   }
 
   delay(target_time);
 
   watchdog_update();
-  return in_future;
 }
 
 // Returns false if the time is in the past
+// Feeds the watchdog while sleeping
 bool sleep_to(Millis target_time) {
   Millis curr = millis();
 
