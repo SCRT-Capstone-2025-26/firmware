8e3654219b63dd8f90fbacf1fba3ee2c4d8d0f64
Llarence <1010kai.t1010@gmail.com>
2026-04-22 13:16:47 -0700

    improve launch detection

--- src/state.h
+++ src/state.h
@@ -31,16 +31,19 @@
 //  Alsso currently this is not implemented, but there could be correlation in recently taken samples so
 //  the error could be based on time
 
+// I don't know why these aren't provided as constants from the library
+// It basically uses a if statement chain on a bunch of floats to figure out
+//  the setting which is strange https://github.com/stm32duino/ISM6HG256X/blob/main/src/ISM6HG256XSensor.cpp#L2705
+#define GYRO_RATE 960.0f
+#define ACC_RATE  960.0f
+
 // 9.80665f is the ISO value of little g
 // TODO: Update these
 #define GRAVITY_ACC       9.80665f
 // This is low for testing
 // The amount of acc from normal gravity required to consider
 //  it a launch
-#define LAUNCH_ACC        0.3f
-// The number of samples that fit the launch criteria
-//  to actually transition to launch
-#define LAUNCH_SAMPLE_REQ 30
+#define LAUNCH_ACC        2.0f
 // A big history can easily take up a lot of the kinda limited ram
 // The seconds of imu data to have in a rolling buffer so that after
 //  launch is detected the first few moments of launch
@@ -48,10 +51,16 @@
 //  the code will have to change
 // This must capture the whole launch so data used in the ROT_HIST_SAMPLES
 //  is not launch data
-#define LAUNCH_HIST_S     0.4f
+#define LAUNCH_HIST_S     2.0f
+// The number of samples that fit the launch criteria
+//  to actually transition to launch this must be smaller than the
+//  history buffer since if it was greater we would lose data
+//  so we use half the buffer size
+// This gives us about 3 detection fails buffer the LAUNCH_HIST_S buffer runs out
+#define LAUNCH_SAMPLE_REQ ((uint32_t)(0.65f * ACC_RATE))
 // We need to determine the rotation before launch from
 //  some accelerometer data so we put that in the circular buffer as well
-#define ROT_HIST_SAMPLES  30
+#define ROT_HIST_SAMPLES  ((uint32_t)(0.5f * ACC_RATE))
 
 // This depends on how the lookup table is made
 // TODO: Determine value
@@ -66,7 +75,7 @@
 // This is the values that the rocket sets the estimated values to if it is booted during flight and there is no flash history
 // TODO: Determine values
 #define UNK_START_HEIGHT 0.0f
-#define UNK_START_VEL 0.0f
+#define UNK_START_VEL    0.0f
 
 // The errors the rocket starts with if it is booted during flight and there is no flash history
 // TODO: Determine values
@@ -77,7 +86,7 @@
 // This is the values that the rocket sets the estimated values to if it is booted during flight
 // TODO: Determine values
 #define BOOT_INC_HEIGHT 0.0f
-#define BOOT_INC_VEL 0.0f
+#define BOOT_INC_VEL    0.0f
 
 // The errors the rocket starts with if it is booted during flight
 // TODO: Determine values
@@ -98,12 +107,6 @@ const Eigen::Vector3f LOCAL_UP(0.0f, 0.0f, 1.0f);
 // This is based on LOCAL_UP (this init should be changed to be dependent on LOCAL_UP)
 // TODO: Check this math see load_flash
 const Eigen::Vector3f RAIL_VEC(0.0f, std::sin(RAIL_ANGLE), std::cos(RAIL_ANGLE));
-
-// I don't know why these aren't provided as constants from the library
-// It basically uses a if statement chain on a bunch of floats to figure out
-//  the setting which is strange https://github.com/stm32duino/ISM6HG256X/blob/main/src/ISM6HG256XSensor.cpp#L2705
-#define GYRO_RATE 960.0f
-#define ACC_RATE  960.0f
 
 struct FlightState {
   Eigen::Quaternionf rot;
@@ -157,6 +160,8 @@ struct RestState {
   //  positives
   // This is a huge ram sink, but should be fine as we have enough and don't use dynamic allocation (except a few strings)
   CircularBuffer<Measurement, BUF_SIZE> buf;
+  // This is pushed after a acc measurement is ejected from the buf so it contains data
+  // older than the buf
   CircularBuffer<Eigen::Vector3f, ROT_HIST_SAMPLES> rot_calib_buf;
 
   // The number of samples in a row that have registered a launch
