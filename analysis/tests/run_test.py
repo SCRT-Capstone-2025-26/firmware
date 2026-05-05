eb523b48dfdf0c1c735d6ee9813d877b11805097
Llarence <1010kai.t1010@gmail.com>
2026-05-05 12:33:28 -0700

    add test runner

---
+++ analysis/tests/run_test.py
@@ -0,0 +1,39 @@
+from collections import namedtuple
+from threading import Thread
+
+from parse import read_iter
+
+State = namedtuple('State', ('time', 'acc', 'hg_acc', 'gyro', 'baro'))
+
+class DataManager():
+    def __init__(self, path):
+        self.path = path
+        self.running = False
+
+
+    def get_current_data(self):
+        if not self.running:
+            raise Exception("Must have started the manager. Please use a with statement")
+
+        # Shallow copy
+        return self.data[:]
+
+
+    def _run(self):
+        for item in self.data_iter:
+            self.data.append(item)
+
+
+    def __enter__(self):
+        self.file = open(self.path, 'rb')
+        self.data_iter = read_iter(self.file)
+        self.data = []
+
+        Thread(target=self._run).start()
+        self.running = True
+
+
+    def __exit__(self):
+        # Should kill the thread
+        self.file.close()
+
